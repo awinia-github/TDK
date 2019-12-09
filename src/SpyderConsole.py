89bf103cb07e1de243a1e97edea153ea7f13a400
Tom Hören <tom.hoeren@micronas.com>
2019-12-09 10:41:58 +0100

    reorganization

--- src/SpyderConsole.py
+++ src/SpyderConsole.py
@@ -6,6 +6,10 @@ Created on Aug 14, 2019
 import os
 
 project_root = str.join(os.path.sep, os.path.split(__file__)[0].split(os.path.sep)[:-1]) 
+# print(project_root)
+
+TDK_src_root = os.path.dirname(__file__)
+# print(TDK_src_root)
 
 project = os.path.join(project_root, 'src')
 resources = os.path.join(project_root, 'resources')
@@ -40,16 +44,11 @@ if __name__ == '__main__':
     print("#    %6.2f MB resources" % round((disk_size(resources)/1000000), 2))
     print("#    %6.2f MB doc & co" % round((disk_size(doc) + disk_size(our_license))/1000000, 2))
     print("#  ", "-"*11)
-    print("#    %6.2f MB total" % round((disk_size(project_root)/1000000), 2))
-    print("#\n# Copy the below text to your (i)python console to get access to the project\n#")
+    print("#    %6.2f MB total\n" % round((disk_size(project_root)/1000000), 2))
     print("import sys, os")
+    exec("import sys, os")
     print("project = r'%s'" % project)
-#     print("ATE_scripts = r'%s'" % ATE_scripts)
-#     print("SCT_scripts = r'%s'" % SCT_scripts)
-#     print("metis_scripts = r'%s'" % metis_scripts)
+    exec("project = r'%s'" % project)
     print("sys.path.insert(0, project)")
-#     print("sys.path.append(ATE_scripts)")
-#     print("sys.path.append(SCT_scripts)")
-#     print("sys.path.append(metis_scripts)")
-#    print("stdf_resources = r'%s'" % stdf_resources)
+    exec("sys.path.insert(0, project)")
     print("os.environ['METIS'] = r'%s'" % metis_path)
