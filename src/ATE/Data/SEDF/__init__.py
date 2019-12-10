4babd20f786f479c6f7b09feb905dfc3e11eb68e
Tom Hören <tom.hoeren@micronas.com>
2019-12-10 17:11:07 +0100

    .

--- src/ATE/Data/SEDF/__init__.py
+++
@@ -1,4 +0,0 @@
-from ATE.Data.Formats.SEDF.utils import SEDFopen as open
-from ATE.Data.Formats.SEDF.records import ImplementedRecords
-for REC in ImplementedRecords:
-    exec("from ATE.Data.Formats.records import %s"%REC)
--- src/ATE/Data/STDF/__init__.py
+++
@@ -1,6 +0,0 @@
-import os
-
-from ATE.data.STDF.records import (ATR, BPS, DTR, EPS, FAR, FTR, GDR, HBR, 
-                                   MIR, MPR, MRR, PCR, PGR, PIR, PLR, PMR, 
-                                   PRR, PTR, RDR, SBR, SDR, TSR, WCR, WIR,
-                                   WRR)
